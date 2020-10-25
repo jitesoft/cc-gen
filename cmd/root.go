cf665f05e6283b8953b28fa319028369b0a35657
Johannes Tegnér <johannes@jitesoft.com>
2020-10-25 14:54:32 +0100

    fix: Moved the generate command to the root command, no need for sub-command.
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,22 +1,160 @@
 package cmd
 
 import (
+    "io"
+    "io/ioutil"
+    "log"
+    "os"
+    "text/template"
+
     "github.com/spf13/cobra"
+
+    "github.com/jitesoft/cc-gen/internal"
+    "github.com/jitesoft/cc-gen/internal/cc"
 )
 
-var rootCmd = &cobra.Command{
+var (
+    from          string = ""
+    createTag     string = ""
+    commitUri     string = ""
+    shouldPrepend bool   = true
+    stdout        bool   = false
+)
+
+var generateCmd = &cobra.Command{
     Use:     "cc-gen",
-    Short:   "Generate a changelog from conventional commits.",
-    Long:    longDesc,
-    Version: "0.0.1",
+    Short:   "Generate a changelog.",
+    Long:    "Generates a changelog in the defined project.",
+    Example: "generate 1.2.3 --from=1.0.2 project/",
+    Args:    cobra.ExactArgs(2),
+    RunE: func(cmd *cobra.Command, args []string) error {
+        path := args[1]
+
+        var fromCommit string
+        if from != "" {
+            fromTag, err := internal.GetTag(path, from)
+            if err != nil {
+                return err
+            }
+            fromCommit = fromTag.Commit
+        } else {
+            fromTag, err := internal.GetLastTag(path)
+            if err != nil || fromTag == nil {
+                cm, err := internal.GetInitialCommit(path)
+                if err != nil {
+                    return err
+                }
+                fromCommit = cm.Hash
+            } else {
+                fromCommit = fromTag.Commit
+            }
+        }
+
+        commits, err := internal.GetCommits(path, fromCommit)
+
+        if err != nil {
+            return err
+        }
+
+        log.Printf("Count %d", len(commits))
+        // Filter
+        c := cc.FilterCommits(commits)
+
+        final := cc.Order(cc.ExtractAll(c))
+
+        var writer io.Writer
+        content := []byte("")
+        if stdout == true {
+            writer = os.Stdout
+        } else {
+            // Stat file to check if it exist.
+            stat, err := os.Stat("CHANGELOG.md")
+            fileExists := false
+            if stat != nil {
+                fileExists = true
+            }
+
+            if fileExists && shouldPrepend == false {
+                writer, err = os.OpenFile("CHANGELOG"+string(args[0])+".md", os.O_WRONLY|os.O_CREATE, 0600)
+            } else {
+                // Store the current content in memory.
+                if fileExists {
+
+                    content, err = ioutil.ReadFile("CHANGELOG.md")
+                    if err != nil {
+                        return err
+                    }
+                }
+
+                writer, err = os.OpenFile("CHANGELOG.md", os.O_RDWR|os.O_CREATE, 0600)
+            }
+            if err != nil {
+                return err
+            }
+        }
+
+        return outputTmpl.Execute(writer, templateData{
+            Commits:   final,
+            Tag:       args[0],
+            CommitUri: commitUri,
+            Extra:     string(content),
+        })
+    },
+}
+
+type templateData struct {
+    Commits   map[string][]*cc.ConventionalCommit
+    Tag       string
+    CommitUri string
+    Extra     string
+}
+
+var outputTmpl *template.Template = template.Must(template.New("markdown").Parse(`# {{ .Tag }}
+{{ range $key, $value := .Commits }}
+{{- if $value }}
+## {{ $key }}
+{{ range $value }}
+  * {{ if $.CommitUri }}[{{- .Hash }}]({{ $.CommitUri }}{{ .Hash }}){{ else }}[{{- .Hash }}]{{ end }} {{ .Header }} <small>By: {{ .Author }} @ {{ .Time }}</small>
+{{- end }}
+{{ end }}
+{{- end }}
+{{ .Extra }}
+`))
+
+func init() {
+    generateCmd.Flags().StringVar(
+        &from,
+        "from",
+        "",
+        "Specific tag to use as start tag for changelog (defaults to latest none-pre release)",
+    )
+    generateCmd.Flags().StringVar(
+        &createTag,
+        "tag",
+        "",
+        "If the generator should also tag the new release with given value (default to empty string)",
+    )
+    generateCmd.Flags().BoolVar(
+        &shouldPrepend,
+        "prepend",
+        true,
+        "If true and a CHANGELOG.md file exists, the tool will prepend to the file, else it will create a new (CHANGELOG_VERSION.md) (default true)",
+    )
+    generateCmd.Flags().BoolVar(
+        &stdout,
+        "stdout",
+        false,
+        "Setting this to true will print the changelog to stdout instead of a file (default false)",
+    )
+    generateCmd.Flags().StringVarP(
+        &commitUri,
+        "commit-uri",
+        "c",
+        "",
+        "Sets base URI to append commit sha to",
+    )
 }
 
 func Execute() error {
-    return rootCmd.Execute()
+    return generateCmd.Execute()
 }
-
-var longDesc = `
-Generates a changelog from conventional commits. 
-
-Depending on flags this application create a changelog spanning between the last two releases (skipping pre-release) 
-and output it in a markdown file or to stdout.`
