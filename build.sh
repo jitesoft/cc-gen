ff4aab9eb8136c9590b583e12122338c9b61384d
Johannes Tegnér <johannes@jitesoft.com>
2021-09-26 11:27:24 +0200

    fix(ci): Updated buildscript and dependencies. Changed command description in root command.
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

--- build.sh
+++ build.sh
@@ -1,43 +1,49 @@
 #! /bin/bash
+export CGO_ENABLED=0
+VERSION="unknown"
+
+if [ ${1+x} ]; then
+  VERSION=$1
+fi
 
 mkdir -p bin
 
 echo "Building linux arm64"
-GOARCH=arm64 GOOS=linux go build
+GOARCH=arm64 GOOS=linux go build -ldflags "-s -w -X github.com/jitesoft/cc-gen/cmd.Version=${VERSION}"
 tar -zc cc-gen -f bin/cc-gen-arm64-linux.tar.gz
 rm cc-gen
 
 echo "Building linux amd64"
-GOARCH=amd64 GOOS=linux go build
+GOARCH=amd64 GOOS=linux go build -ldflags "-s -w -X github.com/jitesoft/cc-gen/cmd.Version=${VERSION}"
 tar -zc cc-gen -f bin/cc-gen-amd64-linux.tar.gz
 rm cc-gen
 
 echo "Building linux arm"
-GOARCH=arm GOOS=linux go build
+GOARCH=arm GOOS=linux go build -ldflags "-s -w -X github.com/jitesoft/cc-gen/cmd.Version=${VERSION}"
 tar -zc cc-gen -f bin/cc-gen-arm-linux.tar.gz
 rm cc-gen
 
 echo "Building darwin amd64"
-GOARCH=amd64 GOOS=darwin go build
+GOARCH=amd64 GOOS=darwin go build -ldflags "-s -w -X github.com/jitesoft/cc-gen/cmd.Version=${VERSION}"
 tar -zc cc-gen -f bin/cc-gen-amd64-darwin.tar.gz
 rm cc-gen
 
 echo "Building darwin arm64"
-GOARCH=arm64 GOOS=darwin go build
+GOARCH=arm64 GOOS=darwin go build -ldflags "-s -w -X github.com/jitesoft/cc-gen/cmd.Version=${VERSION}"
 tar -zc cc-gen -f bin/cc-gen-arm64-darwin.tar.gz
 rm cc-gen
 
 echo "Building windows amd64"
-GOARCH=amd64 GOOS=windows go build
+GOARCH=amd64 GOOS=windows go build -ldflags "-s -w -X github.com/jitesoft/cc-gen/cmd.Version=${VERSION}"
 tar -zc cc-gen.exe -f bin/cc-gen-amd64-windows.tar.gz
 rm cc-gen.exe
 
 echo "Building linux riscv64"
-GOARCH=riscv64 GOOS=linux go build
+GOARCH=riscv64 GOOS=linux go build -ldflags "-s -w -X github.com/jitesoft/cc-gen/cmd.Version=${VERSION}"
 tar -zc cc-gen -f bin/cc-gen-riscv64-linux.tar.gz
 rm cc-gen
 
 echo "Building linux mips64"
-GOARCH=mips64 GOOS=linux go build
+GOARCH=mips64 GOOS=linux go build -ldflags "-s -w -X github.com/jitesoft/cc-gen/cmd.Version=${VERSION}"
 tar -zc cc-gen -f bin/cc-gen-mips64-linux.tar.gz
 rm cc-gen
