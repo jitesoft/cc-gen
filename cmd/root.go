68d003fe73ae315d5f480411f80ff65ebf51874c
Johannes Tegnér <johannes@jitesoft.com>
2021-09-26 11:27:24 +0200

    fix(ci): Updated buildscript and dependencies. Changed command description in root command.
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,7 @@ var rootCmd = &cobra.Command{
 	Version: Version,
 	Short:   "cc-gen is a change log generator for conventional commits.",
 	Long: `cc-gen is a change log generator for conventional commits.
-It scans the directory where it runs for a git repository and
+It scans a given directory for a git repository and
 uses the tags and commits to generate a changelog based on
 the conventional commit standard.`,
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,9 @@
 module github.com/jitesoft/cc-gen
 
-go 1.15
+go 1.16
 
 require (
-	github.com/blang/semver/v4 v4.0.0
 	github.com/go-git/go-git/v5 v5.2.0
 	github.com/spf13/cobra v1.1.1
-	github.com/spf13/viper v1.7.0
 	github.com/stretchr/testify v1.4.0
 )
